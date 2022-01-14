2b4d67faef896ed169753f8f2922be31bbdfbd16
Kyu Park <gypark23@uchicago.edu>
2022-01-13 19:52:24 -0600

    Done 2

--- hw1/2.s
+++ hw1/2.s
@@ -1,4 +1,4 @@
-//2a, Set the value to 0 for registers X18-X25
+//2a, Set the value to 0 for registers X18-X25.
 2a:
     mul x18, x18, x0
     mul x19, x18, x0
@@ -21,7 +21,22 @@
     addi x24, x0, 13
     addi x25, x0, 17
 
-//2c Put the value 0x0 0FFF FFFF FFFF into X18
+//2c Put the value 0x0 0FFF FFFF FFFF into X18.
 2c:
     addi x18, x0, -1
     srli x18, x18, 20
+
+//2g Put the value 0x0 0BA9 8765 4321 into X18.
+2g:
+    addi x18, x0, 0
+    addi x18, x0, 0xBA
+    slli x18, x18, 8
+    addi x18, x18, 0x98
+    slli x18, x18, 8
+    addi x18, x18, 0x76
+    slli x18, x18, 8
+    addi x18, x18, 0x54
+    slli x18, x18, 8
+    addi x18, x18, 0x32
+    slli x18, x18, 4
+    addi x18, x18, 0x1
